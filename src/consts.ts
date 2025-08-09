2f38f315356a61f27971bddc352bdc6b9a5031d5
bx33661 <bx33661@gmail.com>
2025-08-09 14:02:37 +0800

    push

--- src/consts.ts
+++ src/consts.ts
@@ -92,49 +92,63 @@ export const FRIEND_LINKS: FriendLink[] = [
     url: "http://www.bx33661.com",
     description: "见证星辰大海",
     avatar: "https://q1.qlogo.cn/g?b=qq&nk=1811753380&s=640",
-    tags: ["安全"]
+    tags: ["Web"]
   },
   {
     name: "iam0range",
     url: "https://iam0range.github.io/",
     description: "Stay confident",
     avatar: "https://q1.qlogo.cn/g?b=qq&nk=3081999683&s=640",
-    tags: ["安全"]
+    tags: ["pwn"]
   },
   {
     name: "Ewoji",
     url: "https://ewoji.cn/",
     description: "下雨天留客天留我不留",
     avatar: "https://q.qlogo.cn/headimg_dl?dst_uin=1060089371&spec=640&img_type=jpg",
-    tags: ["安全"]
+    tags: ["Web"]
   },
   {
     name: "Unjoke",
     url: "https://unjoke.cn/",
     description: "等雨也等你",
     avatar: "https://q.qlogo.cn/headimg_dl?dst_uin=2801238549&spec=640&img_type=jpg",
-    tags: ["安全"]
+    tags: ["Web"]
   },
   {
     name: "Berial",
     url: "https://berial.cn",
     description: "Stay hungry and cross classes",
     avatar: "https://q1.qlogo.cn/g?b=qq&nk=1409080135&s=640",
-    tags: ["安全"]
+    tags: ["pwn"]
   },
   {
     name: "orxiain.",
     url: "https://orxiain.life",
     description: "",
     avatar: "https://xxx",
-    tags: ["安全"]
+    tags: ["web"]
   },
   {
     name: "Natro92",
     url: "https://natro92.fun",
     description: "Carpe diem.",
     avatar: "https://berial123.oss-cn-beijing.aliyuncs.com/img/860dd94f08a30cf4a8b7fd9685aed42.webp",
-    tags: ["安全"]
+    tags: ["web"]
+  },
+  {
+    name: "m1n9",
+    url: "https://mi1n9.github.io/",
+    description: "苍山负雪，明烛天南",
+    avatar: "https://q.qlogo.cn/headimg_dl?dst_uin=2605742754&spec=640&img_type=jpg",
+    tags: ["Crypto"]
+  },
+  {
+    name: "CrazyCat",
+    url: "https://lh864042219.github.io/",
+    description: "原批",
+    avatar: "https://github.com/LH864042219/PWN-Obsidian/blob/main/picture/QQ.jpg?raw=true",
+    tags: ["pwn"]
   }
 ];
 export const ICON_MAP: IconMap = {
